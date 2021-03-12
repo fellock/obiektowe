366db2941849f029ec2f80868da681d62e6b5a81
fellock <kuba.ostaszewski01@gmail.com>
2021-03-12 13:00:03 +0100

    Zadanie 4 commit

---
+++ LB3/Student.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Student extends Account {
+	private String course;
+	private int startYear;
+
+	public Student(String login, String password, String firstName, String lastName, String course, int startYear) {
+		super(login, password, firstName, lastName);
+		this.course = course;
+		this.startYear = startYear;
+	}
+
+	public int semester() {
+		LocalDate today = LocalDate.now();
+
+		if (today.isAfter(LocalDate.of(startYear, 10, 1)) && today.isBefore(LocalDate.of(startYear + 1, 2, 1))) return 1;
+		else if (today.isAfter(LocalDate.of(startYear + 1, 2, 1)) && today.isBefore(LocalDate.of(startYear + 1, 10, 1))) return 2;
+		else if (today.isAfter(LocalDate.of(startYear + 1, 10, 1)) && today.isBefore(LocalDate.of(startYear + 2, 2, 1))) return 3;
+		else if (today.isAfter(LocalDate.of(startYear + 2, 2, 1)) && today.isBefore(LocalDate.of(startYear + 2, 10, 1))) return 4;
+		else if (today.isAfter(LocalDate.of(startYear + 2, 10, 1)) && today.isBefore(LocalDate.of(startYear + 3, 2, 1))) return 5;
+		else if (today.isAfter(LocalDate.of(startYear + 3, 2, 1)) && today.isBefore(LocalDate.of(startYear + 3, 10, 1))) return 6;
+		else if (today.isAfter(LocalDate.of(startYear + 3, 10, 1)) && today.isBefore(LocalDate.of(startYear + 4, 2, 1))) return 7;
+		else if (today.isAfter(LocalDate.of(startYear + 4, 2, 1)) && today.isBefore(LocalDate.of(startYear + 4, 10, 1))) return 8;
+		else if (today.isAfter(LocalDate.of(startYear + 4, 10, 1)) && today.isBefore(LocalDate.of(startYear + 5, 2, 1))) return 9;
+		else if (today.isAfter(LocalDate.of(startYear + 5, 2, 1)) && today.isBefore(LocalDate.of(startYear + 5, 10, 1))) return 10;
+		else return 0;
+
+		// ROZWIĄZANIE Z ZAJĘĆ PONIŻEJ
+
+		/* LocalDate date = LocalDate.now();
+		Month miesiac = date.getMonth();
+		int rok = date.getYear();
+		if (miesiac.compareTo(Month.OCTOBER) < 0) rok--;
+		int semestr = ((rok - startYear)*2)+1;
+		if(miesiac.compareTo(Month.FEBRUARY) >=0 &&miesiac.compareTo(Month.OCTOBER) < 0) semestr++;
+		return semestr; */
+
+	}
+}
---
+++ LB3/Zadanie4.java
@@ -0,0 +1,10 @@
+/* Napisz klasę Student dziedziczącą po Account. Klasa powinna posiadać dwa pola prywatne: course klasy String i startYear typu int oznaczające kierunek i rok rozpoczęcia studiów na nich. Napisz konstruktor ustawiający wszystkie pola na wartości argumentów.
+
+Napisz także metodę semester podającą numer semestru (1-10) na podstawie pola startYear i aktualnego czasu. Zakładamy, że student aktualnie studiuje, studiuje tylko na jednym kierunku oraz, że semestr zimowy zaczyna się 1.10, a semestr letni 1.02. */
+public class Zadanie4 {
+	public static void main(String[] args) {
+		Student s = new Student("student", "pass", "Rafał", "Kowalski", "Informatyka", 2022);
+
+		System.out.println(s.semester());
+	}
+}
