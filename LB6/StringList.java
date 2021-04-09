2d0e3775e0579a5b2ff38592d609b0c11c9a09ac
fellock <kuba.ostaszewski01@gmail.com>
2021-04-09 12:38:03 +0200

    Zad3 commit

--- LB6/StringList.java
+++ LB6/StringList.java
@@ -44,19 +44,4 @@ public class StringList {
 		head = head.next;
 	}
 
-	/*
-	 * Do klasy listy dodaj referencję na ostatni węzeł oraz metody publiczne:
-	 *
-	 * String last() - zwracającą napis z ostatniego węzła listy, void
-	 * append(String) - wstawiającą napis na koniec listy, void removeLast() -
-	 * usuwającą ostatni węzeł listy.
-	 *
-	 * Uwzględnij rzucanie wyjątku IndexOutOfBoundsException dla pustej listy.
-	 * Napisz testy analogiczne do stworzonych w zadaniu 2 i 3.
-	 */
-
-	public String last() {
-		if (this.head)
-	}
-
 }
