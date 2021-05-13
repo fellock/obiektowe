0c7172183a685be371f237b2333efed1cb928bc0
fellock <kuba.ostaszewski01@gmail.com>
2021-05-14 01:01:24 +0200

    Zadanie 3b commit

--- LB10/Zadanie3.java
+++ LB10/Zadanie3.java
@@ -1,4 +1,9 @@
-import java.awt.event.*;
+import java.awt.FileDialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -24,8 +29,8 @@ public class Zadanie3 {
 		menuBar.setAlignmentX(JMenuBar.LEFT_ALIGNMENT);
 
 		JMenu menu = new JMenu("File");
-		JMenuItem closeMenuItem = new JMenuItem("Close");
 
+		JMenuItem closeMenuItem = new JMenuItem("Close");
 		closeMenuItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -33,6 +38,43 @@ public class Zadanie3 {
 			}
 		});
 
+		JMenuItem openMenuItem = new JMenuItem("Open");
+		openMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				FileDialog fileDialog = new FileDialog(frame, "Choose a file", FileDialog.LOAD);
+				fileDialog.setVisible(true);
+				String filename = fileDialog.getFile();
+				if (filename != null) {
+					try {
+						textArea.read(new BufferedReader(new FileReader(filename)), null);
+						textArea.requestFocus();
+					} catch (Exception x) {
+						x.printStackTrace();
+					}
+				}
+			}
+		});
+
+		JMenuItem saveAsMenuItem = new JMenuItem("Save as");
+		saveAsMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				FileDialog fileDialog = new FileDialog(frame, "Save a file", FileDialog.SAVE);
+				fileDialog.setVisible(true);
+				String filename = fileDialog.getFile();
+				if (filename != null) {
+					try {
+						textArea.write(new FileWriter(fileDialog.getFile()));
+					} catch (Exception x) {
+						x.printStackTrace();
+					}
+				}
+			}
+		});
+
+		menu.add(openMenuItem);
+		menu.add(saveAsMenuItem);
 		menu.add(closeMenuItem);
 		menuBar.add(menu);
 
